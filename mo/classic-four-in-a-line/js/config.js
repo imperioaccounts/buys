2c15f4f023cfbce661474dbf06d208ba7cc286c9
imperioaccounts <felizesfelipe80@gmail.com>
2024-06-07 18:26:25 -0300

    Update config.js

--- mo/classic-four-in-a-line/js/config.js
+++ mo/classic-four-in-a-line/js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "Imperio Accounts",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
